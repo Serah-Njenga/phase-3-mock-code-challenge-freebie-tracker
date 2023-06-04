a657516939e360c62bc0d74df31876f194af5370
Serah-Njenga <serah35.nn@gmail.com>
2023-06-04 14:09:51 +0300

    commit

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,21 @@
 class Company < ActiveRecord::Base
-
+    has_many :freebies
+    has_many :devs, through: :freebies
+    
+    def freebies
+        Freebie.where(company_id: id)
+      end
+      
+      def devs
+        Dev.joins(:freebies).where(freebies: { company_id: id })
+      end
+      
+      def give_freebie(dev, item_name, value)
+        Freebie.create(dev: dev, company: self, item_name: item_name, value: value)
+      end
+      
+      def self.oldest_company
+        order(:founding_year).first
+      end
+      
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,21 @@
 class Dev < ActiveRecord::Base
-
+    has_many :freebies
+    has_many :companies, through: :freebies
+    
+    def freebies
+        Freebie.where(dev_id: id)
+      end
+      
+      def companies
+        Company.joins(:freebies).where(freebies: { dev_id: id })
+      end
+      
+      def received_one?(item_name)
+        freebies.exists?(item_name: item_name)
+      end
+      
+      def give_away(dev, freebie)
+        freebie.update(dev: dev) if freebie.dev == self
+      end
+      
 end
